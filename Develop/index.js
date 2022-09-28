40a2cc7a4b2a2f0f7043df572763d93e4b318b6c
Svetlana Nesterova <sveta59n@yahoo.com>
2022-09-28 13:21:48 -0400

    generating a string of questions

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,74 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const generateMarkdown = require('./develop/utils/generateMarkdown.js');
+
+const { default: inquirer } = require("inquirer");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of the Project'
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Description of the project'
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'What are the instructions for installation?'
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Project usage'
+    },
+    {
+        type: 'input',
+        name: 'license',
+        message: ''
+    },
+    {
+        type: 'input',
+        name: 'contributing',
+        message: ''
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message: ''
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'github'
+    },
+    {
+        type: 'input',
+        name: '',
+        message: ''
+    }
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    return inquirer.prompt(questions)
+    .then((data) => {
+        const mark = generateMarkdown(data)
+        console.log(mark);
+        return data;
+    })
+    .catch((error) => {
+        console.log(error);
+    })
+};
 
 // Function call to initialize app
 init();
+
