ba8763c00859406707d33a63f087d44c520678c8
Svetlana Nesterova <sveta59n@yahoo.com>
2022-09-28 14:22:41 -0400

    creating a new file

--- Develop/index.js
+++ Develop/index.js
@@ -1,8 +1,9 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
-const generateMarkdown = require('./develop/utils/generateMarkdown.js');
+const generateMarkdown = require('./utils/generateMarkdown.js');
+const fs = require('fs');
 
-const { default: inquirer } = require("inquirer");
+// const { default: inquirer } = require("inquirer");
 
 // TODO: Create an array of questions for user input
 const questions = [
@@ -24,12 +25,12 @@ const questions = [
     {
         type: 'input',
         name: 'usage',
-        message: 'Project usage'
+        message: 'Project usage:'
     },
     {
         type: 'input',
         name: 'license',
-        message: ''
+        message: 'License'
     },
     {
         type: 'input',
@@ -44,25 +45,43 @@ const questions = [
     {
         type: 'input',
         name: 'email',
-        message: 'github'
+        message: ''
     },
     {
         type: 'input',
-        name: '',
+        name: 'github',
         message: ''
     }
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileContent) {
+    return new Promise((resolve, reject) => {
+        fs.writeFile('README.md', fileContent, err => {
+            // if there's an error, reject the Promise and send the error to the Promise's `.catch()` method
+            if (err) {
+              reject(err);
+              // return out of the function here to make sure the Promise doesn't accidentally execute the resolve() function as well
+              return;
+            }
+      
+            // if everything went well, resolve the Promise and send the successful data to the `.then()` method
+            resolve({
+              ok: true,
+              message: 'File created!'
+            });
+        });
+    });
+}
+
 
 // TODO: Create a function to initialize app
 function init() {
     return inquirer.prompt(questions)
     .then((data) => {
-        const mark = generateMarkdown(data)
-        console.log(mark);
-        return data;
+        const genMark = generateMarkdown(data)
+        console.log(genMark);
+        return writeToFile();
     })
     .catch((error) => {
         console.log(error);
